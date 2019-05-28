9dd950e22d3e023ad216e5e90e01425907442a4a
djape007 <bpredrag96@gmail.com>
2019-05-28 17:26:19 +0200

    default odgovor da bude json

--- WebApp/WebApp/App_Start/WebApiConfig.cs
+++ WebApp/WebApp/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using Microsoft.AspNet.SignalR;
 using Microsoft.Owin.Security.OAuth;
@@ -27,6 +28,8 @@ namespace WebApp
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
