f7e8826a9b7d8d1f52c4a977bd4a20b4039c932a
Vul4BP <voste69@gmail.com>
2019-05-30 10:41:33 +0200

    zaboravio sam ovo da dodam

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 import { MainService } from './services/main.service';
 import { Bus } from 'src/models/bus';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  //constructor(private _service: MainService) { }
+  @ViewChild('map') mapElement: any;
+  map: google.maps.Map;
 
-  // public funkcija(){
-  //   this._service.getAllBuses()
-  //     .subscribe(data => {
-  //       var buses = new Array<Bus>();
-  //       buses = data;
-  //       console.log(buses);
-  //     })
-  // }
+  constructor(private _service: MainService) { }
+
+  ngOnInit(): void {
+    const mapProperties = {
+         center: new google.maps.LatLng(45.248636, 19.833549),
+         zoom: 14,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+ }
+
+  public funkcija(){
+    this._service.getAllBuses()
+      .subscribe(data => {
+        var buses = new Array<Bus>();
+        buses = data;
+        console.log(buses);
+      })
+  }
 
 }
