bb8a2126ed68fd9214326f9fefea50530ac179e1
Vul4BP <44211967+Vul4BP@users.noreply.github.com>
2019-05-30 18:51:27 +0200

    Dashboard Component

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-app/src/app/dashboard/dashboard.component.ts
+++ angular-app/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from '../services/main.service';
 import { UserLogin } from 'src/models/user-login';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Line } from 'src/models/line';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,22 +11,81 @@ import { UserLogin } from 'src/models/user-login';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private _service: MainService) { }
+  picked_day : string;
+  picked_type : string;
+  
+  lines = [{'LineCode' : '11a'}, {'LineCode' : '4a'},{'LineCode' : '7b'}, {'LineCode' : '5a'}]
+
+  myForm = this._fb.group({
+    type:'',
+    line: this.lines[0].LineCode,
+    day:''
+  });
+
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit() {
+    this.btnDayClick('radni');
+    this.btnTypeClick('gradski');
+  }
+  
+  save() {
+    this.myForm.setValue({  
+      day: this.picked_day,
+      type: this.picked_type,  
+      line: this.myForm.get('line').value
+    });
+    console.log(this.myForm.value);
   }
 
-  public login(usrname: string, pass: string){
-    var user = new UserLogin(usrname, pass);
-    this._service.login(user)
-    .subscribe(
-      data => {
-        console.log(data);
-      },
-      err => {
-        console.log(err);
-      }
-    )
+  public btnDayClick(day: string){
+    let radni = document.getElementById('radni');
+    radni.className = 'square_btn';
+    let sub = document.getElementById('subota');
+    sub.className = 'square_btn';
+    let ned = document.getElementById('nedelja');
+    ned.className = 'square_btn';
+    
+    if(day == 'radni'){
+      radni.className = 'square_btn_active';
+      this.picked_day = 'radni';
+    }
+    else if(day == 'subota'){
+      sub.className = 'square_btn_active';
+      this.picked_day = 'subota';     
+    }
+    else if(day == 'nedelja'){
+      ned.className = 'square_btn_active'; 
+      this.picked_day = 'nedelja';
+    }
   }
+
+  public btnTypeClick(type: string){
+    let gradski = document.getElementById('gradski');
+    gradski.className = 'square_btn';
+    let prigradski = document.getElementById('prigradski');
+    prigradski.className = 'square_btn';
+    
+    if(type == 'gradski'){
+      gradski.className = 'square_btn_active';
+      this.picked_type = 'gradski';
+    }
+    else if(type == 'prigradski'){
+      prigradski.className = 'square_btn_active';
+      this.picked_type = 'prigradski';
+    }
+  }
+  // public login(usrname: string, pass: string){
+  //   var user = new UserLogin(usrname, pass);
+  //   this._service.login(user)
+  //   .subscribe(
+  //     data => {
+  //       console.log(data);
+  //     },
+  //     err => {
+  //       console.log(err);
+  //     }
+  //   )
+  // }
 
 }
