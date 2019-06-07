550780381fdf90d391969fb418bb494148b603bc
Vul4BP <voste69@gmail.com>
2019-06-07 23:57:25 +0200

    Zavrsen Timetable

--- angular-app/src/app/timetable/timetable.component.ts
+++ angular-app/src/app/timetable/timetable.component.ts
@@ -11,15 +11,6 @@ import { MatTableDataSource } from '@angular/material';
   styleUrls: ['./timetable.component.css']
 })
 export class TimetableComponent implements OnInit {
-
-  // picked_day: string;
-  // picked_type: string;
-  // timetables = new Array<Timetable>();
-  // timetabletModel = new Array<Timetable>();
-  // timetableJson = Array<string>();
-  // selectedLineName: string = "";
-  // myForm: FormGroup;
-
   dataSource = new MatTableDataSource();
   displayedColumns: string[] = ['Id', 'Direction'];
 
@@ -117,7 +108,7 @@ export class TimetableComponent implements OnInit {
         polasciPoSatima[satMinut[0]].push(satMinut[1]);
       });
     
-    for (let sat = 4; sat < 24; sat++) {
+    for (let sat = 0; sat < 24; sat++) {
       let satStr = sat.toString().padStart(2, '0');
       if (satStr in polasciPoSatima) {
         let prikazJednogSata = satStr+"|" + polasciPoSatima[satStr].join(",");
@@ -125,149 +116,15 @@ export class TimetableComponent implements OnInit {
       }
     }
 
+    if(timetableJson[0] == "00|00"){
+      let zeroH = timetableJson.shift();
+      timetableJson.push(zeroH);
+    }
     return timetableJson;
   }
-  
 
-  //   this.myForm = this._formBuilder.group({
-  //     day: ['', Validators.required],
-  //     type: ['', Validators.required],
-  //     line: ['', Validators.required]
-  //   });
+  setSelectedDay(timetable: any,selected_day: string){
+    timetable['SelectedDay'] = selected_day;
+  }
+}  
 
-  //   this._sevice.getAllTimetables()
-  //     .subscribe(
-  //       data => {
-  //         this.timetables = data;
-  //         this.timetables.forEach(x=> x.Line.DisplayName = `${x.Line.Id} ${x.Line.Direction}`)
-  //         this.timetabletModel = this.getGradski();
-  //         //this.btnDayClick('radni');
-  //         //this.btnTypeClick('gradski');
-  //       },
-  //       err => {
-  //         console.log(err);
-  //       }
-  //     )
-  // }
-
-  // get f() { return this.myForm.controls; }
-  
-  // save() {
-  //   this.f.day.setValue(this.picked_day);
-  //   this.f.type.setValue(this.picked_type);
-
-  //   if (this.myForm.invalid) {
-  //     this.timetableJson = new Array<string>();
-  //     return;
-  //   }
-
-  //   let selectedLineCode = this.f.line.value;
-  //   let selectedTimeTableObject = this.timetables.find(x => x.Line.Id == selectedLineCode);
-  //   this.selectedLineName = selectedTimeTableObject.Line.DisplayName;
-
-  //   this.displayLineOnMap(selectedTimeTableObject.Line.Id);
-
-  //   let timesJson = JSON.parse(selectedTimeTableObject.Times);
-  //   let selectedDayTimesJson = new Array<string>();
-  //   if(this.picked_day == "Radni_dan")
-  //     selectedDayTimesJson = timesJson['Radni_dan'];
-  //   else if(this.picked_day == "Subota")
-  //     selectedDayTimesJson = timesJson['Subota'];
-  //   else if(this.picked_day == "Nedelja")
-  //     selectedDayTimesJson = timesJson['Nedelja'];
-
-  //   this.timetableJson = new Array<string>();
-
-  //   let polasciPoSatima = {};
-  //   selectedDayTimesJson.forEach(
-  //     (item, indeks) => {
-  //       let satMinut = item.split(":");
-  //       if (!(satMinut[0] in polasciPoSatima)) {
-  //         polasciPoSatima[satMinut[0]] = new Array<string>();
-  //       }
-
-  //       polasciPoSatima[satMinut[0]].push(satMinut[1]);
-  //     });
-    
-  //   for (let sat = 4; sat < 24; sat++) {
-  //     let satStr = sat.toString().padStart(2, '0');
-  //     if (satStr in polasciPoSatima) {
-  //       let prikazJednogSata = satStr+"|" + polasciPoSatima[satStr].join(",");
-  //       this.timetableJson.push(prikazJednogSata);
-  //     }
-  //   }
-  // }
-
-  // displayLineOnMap(lineId: any) {
-  //   this._parent.DisplayLineOnMap(lineId);
-  // }
-  
-  // getPrigradski(): Array<Timetable>{
-  //   return this.timetables
-  //   .filter(
-  //     x => (Number)(x.Line.Id.replace('A','').replace('B','')) > 20
-  //   ).filter(
-  //     x => x.Times.includes(this.picked_day)
-  //   ).sort(
-  //     (x,y) => (Number)(x.Line.Id.replace('A','').replace('B','')) - (Number)(y.Line.Id.replace('A','').replace('B',''))
-  //   );
-  // }
-
-  // getGradski(): Array<Timetable>{
-  //   return this.timetables
-  //   .filter(
-  //     x => (Number)(x.Line.Id.replace('A','').replace('B','')) < 20
-  //   ).filter(
-  //     x => x.Times.includes(this.picked_day)
-  //   )
-  //   .sort(
-  //     (x,y) => (Number)(x.Line.Id.replace('A','').replace('B','')) - (Number)(y.Line.Id.replace('A','').replace('B',''))
-  //   );
-  // }
-
-  // public btnDayClick(day: string){
-  //   let radni = document.getElementById('radni');
-
-  //   radni.className = 'square_btn';
-  //   let sub = document.getElementById('subota');
-  //   sub.className = 'square_btn';
-  //   let ned = document.getElementById('nedelja');
-  //   ned.className = 'square_btn';
-    
-  //   if(day == 'radni'){
-  //     radni.className = 'square_btn_active';
-  //     this.picked_day = 'Radni_dan';
-  //     this.myForm.reset();
-  //   }
-  //   else if(day == 'subota'){
-  //     sub.className = 'square_btn_active';
-  //     this.picked_day = 'Subota';   
-  //     this.myForm.reset();  
-  //   }
-  //   else if(day == 'nedelja'){
-  //     ned.className = 'square_btn_active'; 
-  //     this.picked_day = 'Nedelja';
-  //     this.myForm.reset();
-  //   }
-  // }
-
-  // public btnTypeClick(type: string){
-  //   let gradski = document.getElementById('gradski');
-  //   gradski.className = 'square_btn';
-  //   let prigradski = document.getElementById('prigradski');
-  //   prigradski.className = 'square_btn';
-    
-  //   if(type == 'gradski'){
-  //     gradski.className = 'square_btn_active';
-  //     this.picked_type = 'gradski';
-  //     this.timetabletModel = this.getGradski();
-  //     this.myForm.reset();
-  //   }
-  //   else if(type == 'prigradski'){
-  //     prigradski.className = 'square_btn_active';
-  //     this.picked_type = 'prigradski';
-  //     this.timetabletModel = this.getPrigradski();
-  //     this.myForm.reset();
-  //   }
-  // }
-}
